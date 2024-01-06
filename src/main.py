5b2564c15b3422fd6e313657fb7974f73201ded5
takehiro-ichimura <sprollhrmk@gmail.com>
2024-01-07 03:26:34 +0900

    更新

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ def main():
     exe_path = os.path.dirname(os.path.abspath(sys.argv[0]))
     textModel = TextModel.create(exe_path + '/../input/input.txt', exe_path + '/../templates/template.html')
     textModel.export(parseMode=ParseMode.USE_BR, output_path=exe_path + '/../output/')
+    print('##### FINISHED')
 
 if __name__ == "__main__":
     main()
